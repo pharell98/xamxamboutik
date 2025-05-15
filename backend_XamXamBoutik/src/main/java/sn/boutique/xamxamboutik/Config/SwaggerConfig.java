439da88e9b73f195a59b3f9d52ab3bf9755ded8e
pharell sow <sowpharell98@hotmail.com>
2025-05-15 11:46:49 +0000

    Initial commit for xamxamboutik with backend and frontend

--- backend_XamXamBoutik/src/main/java/sn/boutique/xamxamboutik/Config/SwaggerConfig.java
+++ backend_XamXamBoutik/src/main/java/sn/boutique/xamxamboutik/Config/SwaggerConfig.java
@@ -1,4 +1,5 @@
-package sn.boutique.xamxamboutik.Config;
+package sn.boutique.xamxamboutik.config;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -9,31 +10,32 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
+
 @Configuration
 @OpenAPIDefinition
 public class SwaggerConfig {
+
     @Bean
     public OpenAPI apiDoc() {
         return new OpenAPI()
                 .addServersItem(new Server()
-                        .url("http://localhost:8080/api/v1")
+                        .url("/api/v1")
                         .description("Serveur local avec context-path /api/v1"))
                 .info(new Info()
-                        .title("Darou Salam API")
-                        .description("API de Gestion de Stock Darou Salam")
+                        .title("Xamxamboutik API")
+                        .description("API de Gestion de Stock Xamxamboutik")
                         .version("V.1.0.0")
                         .license(new License()
                                 .name("Apache 2.0")
                                 .url("http://springdoc.org")))
                 .components(new Components()
-                        .addSecuritySchemes("bearer-jwt",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")))
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
                 .security(List.of(
-                        new SecurityRequirement().addList("bearer-jwt")
-                ));
+                        new SecurityRequirement().addList("bearer-jwt")));
     }
 }
--- backend_XamXamBoutik/src/main/java/sn/boutique/xamxamboutik/security/config/SecurityConfig.java
+++ backend_XamXamBoutik/src/main/java/sn/boutique/xamxamboutik/security/config/SecurityConfig.java
@@ -1,4 +1,5 @@
 package sn.boutique.xamxamboutik.security.config;
+
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -16,91 +17,112 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import sn.boutique.xamxamboutik.security.constants.SecurityConstants;
 import sn.boutique.xamxamboutik.security.jwt.JwtTokenFilter;
 import sn.boutique.xamxamboutik.security.service.CustomUserDetailsService;
+
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
     private final JwtTokenFilter jwtTokenFilter;
     private final CustomUserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(cors -> {
+                    cors.configurationSource(corsConfigurationSource());
+                    System.out.println("Applying CORS configuration to HTTP requests");
+                })
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers(SecurityConstants.PublicUrls.SWAGGER_URLS).permitAll()
                         .requestMatchers("/ws/**").permitAll()
-                        .requestMatchers("/approvisionnement/**", "/categories/**", "/produits/**","/statistiques/**").hasRole("GESTIONNAIRE")
+                        .requestMatchers("/approvisionnement/**", "/categories/**", "/produits/**")
+                        .hasRole("GESTIONNAIRE")
                         .anyRequest().authenticated()
                 )
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .exceptionHandling(exceptions -> exceptions
-                        .authenticationEntryPoint((request, response, authException) -> customUnauthorizedResponse(response))
-                        .accessDeniedHandler((request, response, accessDeniedException) -> customForbiddenResponse(response))
+                        .authenticationEntryPoint((req, res, ex) ->
+                                customUnauthorizedResponse(res))
+                        .accessDeniedHandler((req, res, ex) ->
+                                customForbiddenResponse(res))
                 )
                 .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtTokenFilter,
+                        UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
+
     @Bean
-    public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
-        var configuration = new org.springframework.web.cors.CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList(
-                SecurityConstants.HEADER_STRING,
-                "Content-Type",
-                "Authorization",
-                "x-client-type"
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration cfg = new CorsConfiguration();
+        cfg.setAllowedOrigins(List.of(
+                "https://darou-salam.xamxamboutik.shop"
         ));
-        configuration.setExposedHeaders(List.of("Authorization"));
-        configuration.setAllowCredentials(true);
-        var source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        cfg.setAllowedMethods(List.of(
+                "GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"
+        ));
+        cfg.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
+        cfg.setExposedHeaders(List.of("Authorization"));
+        cfg.setAllowCredentials(true);
+
+        System.out.println("CORS configuration applied for origins: " + cfg.getAllowedOrigins());
+        UrlBasedCorsConfigurationSource src = new UrlBasedCorsConfigurationSource();
+        src.registerCorsConfiguration("/**", cfg);
+        return src;
     }
+
     @Bean
     public AuthenticationProvider authenticationProvider() {
-        var authProvider = new DaoAuthenticationProvider();
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
         authProvider.setUserDetailsService(userDetailsService);
         authProvider.setPasswordEncoder(passwordEncoder);
         return authProvider;
     }
+
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-    private void customUnauthorizedResponse(HttpServletResponse response) throws IOException {
+
+    private void customUnauthorizedResponse(HttpServletResponse response)
+            throws IOException {
         response.setStatus(HttpStatus.UNAUTHORIZED.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        String body = """
-            {
-              "success": false,
-              "message": "Vous n'êtes pas connecté ou votre token est invalide",
-              "errorCode": "UNAUTHORIZED"
-            }
-            """;
-        response.getWriter().write(body);
+        response.getWriter().write("""
+                {
+                  "success": false,
+                  "message": "Vous n'êtes pas connecté ou votre token est invalide",
+                  "errorCode": "UNAUTHORIZED"
+                }
+                """);
     }
-    private void customForbiddenResponse(HttpServletResponse response) throws IOException {
+
+    private void customForbiddenResponse(HttpServletResponse response)
+            throws IOException {
         response.setStatus(HttpStatus.FORBIDDEN.value());
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        String body = """
-            {
-              "success": false,
-              "message": "Vous êtes connecté, mais vous n'avez pas le rôle requis pour accéder à cette ressource",
-              "errorCode": "ACCESS_DENIED"
-            }
-            """;
-        response.getWriter().write(body);
+        response.getWriter().write("""
+                {
+                  "success": false,
+                  "message": "Vous êtes connecté, mais vous n'avez pas le rôle requis pour accéder à cette ressource",
+                  "errorCode": "ACCESS_DENIED"
+                }
+                """);
     }
 }
