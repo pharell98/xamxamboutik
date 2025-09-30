24210f1f29eb114edc7830570fa618b27d904ab4
pharell sow <sowpharell98@hotmail.com>
2025-09-30 20:50:07 +0000

    feat: mise à jour CaisseScheduler pour fermeture automatique de caisse

--- backend/src/main/java/sn/boutique/xamxamboutik/Config/CaisseScheduler.java
+++ backend/src/main/java/sn/boutique/xamxamboutik/Config/CaisseScheduler.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import sn.boutique.xamxamboutik.Service.statistique.CaisseInternalService;
 
-import java.util.Map;
+
 
 /**
  * Scheduler pour la gestion automatique de la caisse
